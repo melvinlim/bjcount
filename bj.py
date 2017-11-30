ee64b76a06ccb67566605a2517dbf772f00c2fe6
Melvin Lim <melvinlim@MacBook.local>
2017-11-30 11:56:26 -0800

    modify initial settings from within program

--- bj.py
+++ bj.py
@@ -1,26 +1,22 @@
 from table import *
-fullyAutomated=True
-updateInterval=1000
+print 'update interval?\n[1000]',
+x=raw_input()
+try:
+	updateInterval=int(x)
+except:
+	updateInterval=1000
 t=Table(nPlayers=4,nDecks=2,bankroll=100000,minBet=10,bjmultiplier=1.5)
 #t=Table(nPlayers=4,nDecks=2,bankroll=100,minBet=10,bjmultiplier=1.5)
 handsPlayed=0
 while True:
-	t.round()
-	t.status()
-	handsPlayed+=1
-	if fullyAutomated:
-		print 'hands played: '+str(handsPlayed)
-		if handsPlayed%updateInterval==1:
-			print 'continue? (y/n)\t',
-			state=raw_input()
-			if state=='':
-				pass
-			elif state=='n':
-				break
-	else:
+	print 'hands played: '+str(handsPlayed)
+	if handsPlayed%updateInterval==0:
 		print 'continue? (y/n)\t',
 		state=raw_input()
 		if state=='':
 			pass
 		elif state=='n':
 			break
+	t.round()
+	t.status()
+	handsPlayed+=1
